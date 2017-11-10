df3f0e951a14f36204c14ec58d5e31fcc7c5b581
Reka Kulinyi <reekaa85@gmail.com>
2017-11-10 11:18:42 +0100

    seed ingredients with api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
 Ingredient.destroy_all
-ingredients = ["lemon", "ice", "coca", "vodka", "whiskey"]
 
-ingredients.each do |i|
-  Ingredient.create(name: i)
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_serialized = open(url).read
+
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
 end
-
