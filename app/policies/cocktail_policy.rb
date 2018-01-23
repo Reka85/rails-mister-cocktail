b966e5aa2eb3f67c44988ccb9c98a421fba31995
Reka Kulinyi <reekaa85@gmail.com>
2018-01-23 14:12:04 +0100

    cocktail policy

--- app/policies/cocktail_policy.rb
+++ app/policies/cocktail_policy.rb
@@ -5,7 +5,6 @@ class CocktailPolicy < ApplicationPolicy
     end
   end
 
-
   # anyone can create a cocktail
   # new? is not necessary here, see in app_policy.rb: new? calls create? => new? will also return true
   def create?
