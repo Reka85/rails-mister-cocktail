53d32d6d9ae43e362fcc7527c027439bf9f77780
Reka Kulinyi <reekaa85@gmail.com>
2018-01-23 17:28:43 +0100

    refactor dose policy

--- app/policies/dose_policy.rb
+++ app/policies/dose_policy.rb
@@ -7,11 +7,17 @@ class DosePolicy < ApplicationPolicy
 
   # only the creator of the cocktail can create dose for the cocktail
   def create?
-    user == record.cocktail.user
+    is_user_the_owner?
   end
 
   # only creator of dose can delete it
   def destroy?
+    is_user_the_owner?
+  end
+
+  private
+
+  def is_user_the_owner?
     user == record.cocktail.user
   end
 end
